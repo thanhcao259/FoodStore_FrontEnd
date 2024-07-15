8ffcadf4f72ec87e9e3085eb78db520ea1543f87
thanhcao259 <thanhcao9501@gmail.com>
2024-07-15 15:42:03 +0700

    update axios admin - all products

--- foodStore/src/api/listProducts.js
+++ foodStore/src/api/listProducts.js
@@ -7,7 +7,7 @@ const listProducts= {
         return axiosClient.get(url);
     },
     getAllByAdmin(){
-        const url = '/admin/products/get-all';
+        const url = '/products/get-all';
         return axiosAdmin.get(url);
     },
     delete(id){
