d8afb3dcadf4f49ec5e25160e536bf16228ec8d1
thanhcao259 <thanhcao9501@gmail.com>
2024-07-16 10:03:14 +0700

    axios register

--- foodStore/src/components/register.jsx
+++ foodStore/src/components/register.jsx
@@ -30,13 +30,15 @@ function Register(){
                 };
                 const response = registerApi.add(postData);
                 console.log("data ", postData);
-                if(response.status === 201 ){
+                if(response.status === 201 || response.status === 200){
                     navigate('/verify_register');
                     toast.success("Register success!");
                 } if (response.status === 409) {
                     toast.error("Email already exists");
                 } else {
                     toast.error("Register failed");
+                    console.log("Another err");
+        
                 }
                 
             } catch (error) {
